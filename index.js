35bae5669e91dfc8f460cb238e44598163c446ee
komal8683 <komal.j8683@gmail.com>
2025-09-26 16:18:39 +0530

    git server push

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ const app = express();
 app.use(express.json());
 app.use(cookieparser())
 app.use(express.static(path.join(__dirname, "dist")))
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({
+    origin:
+        process.env.NODE_ENV === 'development' ?
+            "http://localhost:5173" : "https://swiggy-clone-pi-eight.vercel.app", credentials: true
+}));
 app.use(express.static("upload"))
 
 
@@ -21,8 +25,8 @@ app.use("/api/hotelDash", require("./routes/hotelDash.route"))
 
 
 app.use("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"))
-    // res.status(404).json({ message: "Resource Not Found" });
+    // res.sendFile(path.join(__dirname, "dist", "index.html"))
+    res.status(404).json({ message: "Resource Not Found" });
 });
 app.use((err, req, res, next) => {
     console.log(err);
